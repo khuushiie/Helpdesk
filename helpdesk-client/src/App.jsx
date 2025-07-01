10e3ba66d06baf3fa79fea13f7b97a55122a22bb
khuushiie <khushm004@gmail.com>
2025-07-01 11:02:05 +0530

    Initial commit: Setup frontend structure

---
+++ helpdesk-client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <h1 className="text-primary fw-bold">Bootstrap CDN is working!</h1>
+    </div>
+  );
+}
+
+export default App;
