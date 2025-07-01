ab4eb556e7d45ca4273df8cbc4988284d2568276
khuushiie <khushm004@gmail.com>
2025-07-01 11:22:03 +0530

    Add SignIn page UI

--- helpdesk-client/src/App.jsx
+++ helpdesk-client/src/App.jsx
@@ -1,7 +1,8 @@
+import SignIn from './pages/SignIn';
 function App() {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-      <h1 className="text-primary fw-bold">Bootstrap CDN is working!</h1>
+    <div>
+      <SignIn />
     </div>
   );
 }
